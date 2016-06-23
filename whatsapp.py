65be0eb842b0a048f42a3c2fb4e6cdbc06db7875
Gurunath <guru9794@gmail.com>
2016-06-24 00:47:16 +0530

    Updated to fix issue in searching of contacts
    Updated logic to search contact name. Replaced method of getting first element from search result to getting Contact from exact contact name.Currently, exact contact name has to be replaced with CONTACT NAME

--- whatsapp.py
+++ whatsapp.py
@@ -8,17 +8,20 @@ import os
 
 os.environ["webdriver.chrome.driver"]=chromedriver     #Comment if you want to use Firefox
 driver=webdriver.Chrome(chromedriver)                 #Comment if you want to use Firefox
+driver=webdriver.Chrome(chromedriver)
 driver.get("http://web.whatsapp.com")
-wait = ui.WebDriverWait(driver, 60) # timeout after 10 seconds
+wait = ui.WebDriverWait(driver, 20)
 results = wait.until(lambda driver:driver.find_element_by_css_selector("#pane-side > div > div > div > div:nth-child(1) > div > div > div.chat-body"))
 search=driver.find_elements_by_css_selector("#side > div.search-container > div > label > input")
 searchbox=search[0]
 searchbox.send_keys("CONTACT NAME")
 driver.implicitly_wait(5) # seconds
 results = wait.until(lambda driver:driver.find_element_by_css_selector("#pane-side > div > div > div > div > div > div > div.chat-body"))
-name=driver.find_elements_by_css_selector("#pane-side > div > div > div > div > div > div > div.chat-body")
-name[0].click()
+wait.until(lambda driver:driver.find_element_by_css_selector("span[title='CONTACT NAME']"))
+name=driver.find_element_by_css_selector("span[title='CONTACT NAME']")
+name.click()
 text=driver.find_element_by_css_selector("#main > footer > div > div.input-container > div > div.input")
-text.send_keys("Hello,Sent Automoatically !!! ")
+text.send_keys("Good Night !! Sent from WhatsppIt :)")
 text.send_keys("\n")
 #driver.quit()
+
